eaadda7e4281764256a1221a2696206ccb56335a
Don Huff <dhuff@15x10.com>
2014-10-29 08:14:04 -0500

    Initial Commit

---
+++ Bubble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bubble
+//
+//  Created by Don Huff on 10/29/14.
+//  Copyright (c) 2014 15 by 10, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bubble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bubble
+//
+//  Created by Don Huff on 10/29/14.
+//  Copyright (c) 2014 15 by 10, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
